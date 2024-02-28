e56d2177f9c99a7383ea9316591b036897b79f6b
gmolnar2022 <geza.molnar2@uni-corvinus.hu>
2024-02-28 15:07:02 +0100

    feladatok vége

---
+++ SQLQuery_2.sql
@@ -0,0 +1,17 @@
+/*1.  */
+
+SELECT *,
+       -- új oszlop
+       IIF(MONTH(METTOL) IN (6, 7, 8), 'Igen', 'Nem') AS 'Nyári-e'
+FROM foglalas
+WHERE UGYFEL_FK = 'laszlo2' AND GYERMEK_SZAM = 0
+
+--2
+SELECT szh.TIPUS, 
+       YEAR(f.METTOL) AS 'Év', 
+       MONTH(f.METTOL) AS 'Hónap', 
+       COUNT(*)
+FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID 
+                JOIN Szallashely szh ON sz.SZALLAS_FK = szh.SZALLAS_ID
+WHERE DATEDIFF(day, f.METTOL, f.MEDDIG) >=5 
+GROUP BY szh.TIPUS, YEAR(f.METTOL), MONTH(f.METTOL)
